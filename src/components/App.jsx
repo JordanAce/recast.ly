04c70d6b296c4ddd91491d5de3acf613ac2d74ee
Jason Carini <carinij@gmail.com>
2021-02-04 22:15:13 -0800

    Complete connecting Video List and Player components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,23 +3,45 @@ import exampleVideoData from '/compiled/src/data/exampleVideoData.js';
 import VideoPlayer from '/compiled/src/components/VideoPlayer.js';
 
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullVideoList: exampleVideoData,
+      currentVideo: exampleVideoData[0],
+      testFunction: [1, 2],
+    };
+
+    this.changeVideo = this.changeVideo.bind(this);
+
+  }
+
+  changeVideo(newVideo) {
+    this.setState({currentVideo: newVideo});
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div onClick={this.changeVideo}><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><em>videoPlayer</em><VideoPlayer video={this.state.currentVideo} /></h5></div>
+          </div>
+          <div className="col-md-5">
+            <div><h5><em>videoList</em><VideoList videos={this.state.fullVideoList} testFunction={this.state.testFunction} clickFunction={this.changeVideo} /></h5></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em><VideoPlayer video={exampleVideoData[0]}/></h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em><VideoList videos={exampleVideoData}/></h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -4,9 +4,10 @@ var VideoList = (props) => {
 
   let allVideos = [];
 
+
   for (let i = 0; i < props.videos.length; i++) {
     allVideos.push(
-      <VideoListEntry video={props.videos[i]} />
+      <VideoListEntry video={props.videos[i]} clickFunction={props.clickFunction} />
     );
   }
 
