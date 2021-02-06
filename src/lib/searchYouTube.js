fee2c7d0131848b74404ed0755b2a2aadcac30cb
Jason Carini <carinij@gmail.com>
2021-02-06 11:50:05 -0800

    Complete creating reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,11 +1,14 @@
 var searchYouTube = (options, callback) => {
-  let videosArray = $.get("https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=" + (options.max ? options.max : 5) + "&q=" + options.query + "&videoEmbeddable=true&key=" + options.key);
-  console.log("https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=" + (options.max ? options.max : 5) + "&q=" + options.query + "&videoEmbeddable=true&key=" + options.key);
+  let videosArray = [];
+  $.get("https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=" + (options.max ? options.max : 5) + "&q=" + options.query + "&videoEmbeddable=true&key=" + options.key, function(data) {
+    return callback(data.items);
+  });
+
+
+
+  //console.log("https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=" + (options.max ? options.max : 5) + "&q=" + options.query + "&videoEmbeddable=true&key=" + options.key);
   //console.log(videosArray.responseJSON.items);
-  console.log(callback);
-  console.log(videosArray);
-  console.log(options.key);
-  return callback(videosArray);
+
 };
 
 export default searchYouTube;
